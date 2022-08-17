dfcf1bb89e109210108e337994fa746cc39e425a
Lê Thái Sơn <lethaison04011999@gmail.com>
2022-08-17 12:53:02 +0700

    deploy project 2

--- src/index.js
+++ src/index.js
@@ -1,14 +1,37 @@
+//config nodejs with express
+
 const express = require('express');
+const path = require('path');
 const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+
 const app = express();
 const port = 3000;
 
-app.use(morgan('combined'));
-//route
-app.get('/', (req, res) => {
-	res.send('Hello World!');
-});
+//public static image
+app.use(express.static(path.join(__dirname, 'public')));
 
+//HTTP Logger
+app.use(morgan('combined'));
+
+//template engine
+
+app.engine(
+	'hbs',
+	handlebars.engine({
+		extname: '.hbs',
+	})
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+//route
+
+app.get('/', (req, res) => {
+	res.render('home');
+});
+app.get('/news', (req, res) => {
+	res.render('news');
+});
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
 });
